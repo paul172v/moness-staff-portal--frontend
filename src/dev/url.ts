947a8d6ab049c1008ef7583d049066fae97e8540
paul172v <paul172v@aol.com>
2025-06-22 23:51:17 +0100

    bug fixing forgot password email

--- src/dev/url.ts
+++ src/dev/url.ts
@@ -1,3 +1,4 @@
-const URL_ADDRESS = "http://localhost:5000/api/v1";
+const URL_ADDRESS = "https://moness-backend-4c94e04e2f82.herokuapp.com/api/v1";
+// const URL_ADDRESS = "http://localhost:5000/api/v1";
 
 export default URL_ADDRESS;
